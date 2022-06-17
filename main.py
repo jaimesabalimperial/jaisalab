c218ec18176cde2bb68667741a4b68dc50cc05a9
Jaime Sabal <js921@ic.ac.uk>
2022-06-17 16:24:51 +0100

    some more juicy changes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from jaisalab.experiments.trpo_inv_management import trpo_inv_mng_backlog
+from jaisalab.experiments.trpo_backlog import trpo_inv_mng_backlog
 
 def test_trpo_backlog():
     trpo_inv_mng_backlog(seed=1)
+
+def test_cpo_backlog():
+    pass
 
 if __name__ == '__main__':
     test_trpo_backlog()
