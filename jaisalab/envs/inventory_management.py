47ce1cc1d5654af6b9d48dfaaec3d1043978e238
Jaime Sabal <js921@ic.ac.uk>
2022-07-29 16:01:00 +0200

    refactored functions

--- jaisalab/envs/inventory_management.py
+++ jaisalab/envs/inventory_management.py
@@ -20,21 +20,7 @@ from scipy.stats import *
 #jaisalab
 from jaisalab.envs.saute_env import saute_env
 from jaisalab.envs.safe_env import SafeEnv
-
-def assign_env_config(self, kwargs):
-    for key, value in kwargs.items():
-        setattr(self, key, value)
-    if hasattr(self, 'env_config'):
-        for key, value in self.env_config.items():
-            # Check types based on default settings
-            if hasattr(self, key):
-                if type(getattr(self,key)) == np.ndarray:
-                    setattr(self, key, value)
-                else:
-                    setattr(self, key,
-                        type(getattr(self, key))(value))
-            else:
-                raise AttributeError(f"{self} has no attribute, {key}")
+from jaisalab.utils.env import assign_env_config
 
 class InvManagementMasterEnv(gym.Env):
     '''
--- jaisalab/utils/env.py
+++ jaisalab/utils/env.py
@@ -27,3 +27,18 @@ def env_setup(env):
     env = normalize(GymEnv(env))
 
     return env
+
+def assign_env_config(self, kwargs):
+    for key, value in kwargs.items():
+        setattr(self, key, value)
+    if hasattr(self, 'env_config'):
+        for key, value in self.env_config.items():
+            # Check types based on default settings
+            if hasattr(self, key):
+                if type(getattr(self,key)) == np.ndarray:
+                    setattr(self, key, value)
+                else:
+                    setattr(self, key,
+                        type(getattr(self, key))(value))
+            else:
+                raise AttributeError(f"{self} has no attribute, {key}")
