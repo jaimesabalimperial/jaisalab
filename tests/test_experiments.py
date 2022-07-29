effab39cfb0f75b3e6095e1167427b05d55daa63
Jaime Sabal <js921@ic.ac.uk>
2022-07-29 16:04:40 +0200

    modified tests

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -1,5 +1,9 @@
 import pytest
-from jaisalab.experiments import trpo_inv_mng_backlog
+from jaisalab.experiments import (trpo_backlog, cpo_backlog, 
+                                  saute_trpo_backlog)
 
 if __name__ == '__main__':
-    trpo_inv_mng_backlog(seed=1)
+    trpo_backlog(seed=1)
+    cpo_backlog(seed=1)
+    saute_trpo_backlog(seed=1)
+
