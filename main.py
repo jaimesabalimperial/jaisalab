cfff017298765ee453d32d337db4445b3ad96698
Jaime Sabal <js921@ic.ac.uk>
2022-08-03 17:24:13 +0200

    trying to get paperspace to work

--- main.py
+++ main.py
@@ -17,18 +17,18 @@ def test_dcpo_backlog():
 
 
 def plot_experiment():
-    fdir = ['trpo_backlog_5', 'cpo_backlog_2', 'saute_trpo_backlog_19', 'dcpo_backlog_34']
-    plotter = Plotter(fdir=fdir)
+    #fdir = ['trpo_backlog_5', 'cpo_backlog_2', 'saute_trpo_backlog_19', 'dcpo_backlog_34']
+    plotter = Plotter(fdir=None)
     #plotter.plot_returns()
     #plotter.plot_constraint_vals()
     #plotter.plot_costs()
     #plotter.plot_gaussian_progression(num_points=400)
     #plotter.plot_final_distribution()
-    plotter.plot_quantiles_progression(interval=10)
+    #plotter.plot_quantiles_progression(interval=10)
 
 if __name__ == '__main__':
-    #test_trpo_backlog()
-    test_cpo_backlog()
+    test_trpo_backlog()
+    #test_cpo_backlog()
     #test_saute_trpo_backlog()
     #test_dcpo_backlog()
-    plot_experiment()
+    #plot_experiment()
