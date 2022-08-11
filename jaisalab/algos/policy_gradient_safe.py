now really fixed cpo, had to rescale c by the average trajectory length

jaisalab/algos/policy_gradient_safe.py
@@ -221,7 +221,7 @@ class PolicyGradientSafe(VPG):
                                                 for safety_reward in eps.padded_safety_rewards]))
         
         #necessary as per jachiam's cpo repository
-        self.safety_rescale = len(safety_rewards) / sum([len(path) for path in safety_rewards])
+        self.rescale_factor = np.mean([len(path) for path in safety_rewards])
 
         #calculate safety baseline
         with torch.no_grad():

jaisalab/safety_constraints/inventory_constraint.py
@@ -17,7 +17,7 @@ class SoftInventoryConstraint(BaseConstraint):
         baseline_optimizer (torch.optim.Optimizer): Safety baseline optimizer. 
         penalty (float): Penalty for violating constraint. 
     """
-    def __init__(self, max_value=25., baseline=None, 
+    def __init__(self, max_value=15., baseline=None, 
                  baseline_optimizer=None, penalty=1., 
                  discount=1., **kwargs):
         super().__init__(max_value, baseline=baseline, 